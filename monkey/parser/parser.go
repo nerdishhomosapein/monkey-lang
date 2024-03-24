3d81de2ef2771514769d7f192afa95c59383455d
kumar prateek <kumarprateek902@gmail.com>
2024-03-24 19:55:35 +0530

    Added parser tests for return statements

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/nerdishhomosapein/monkey-lang/ast"
 	"github.com/nerdishhomosapein/monkey-lang/lexer"
 	"github.com/nerdishhomosapein/monkey-lang/token"
@@ -11,6 +13,7 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 func (p *Parser) nextToken() {
@@ -36,6 +39,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
@@ -80,8 +84,18 @@ func (p *Parser) ParseProgram() *ast.Program {
 
 func New(l *lexer.Lexer) *Parser {
 
-	p := &Parser{l: l}
+	p := &Parser{l: l,
+		errors: []string{}}
 	p.nextToken()
 	p.nextToken()
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
